d9160e1eade6de38c44350c917809c6e8eb9640a
Frank Chang <yuehminchang@gmail.com>
2024-12-06 21:07:00 +0800

    Update_Test2

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -22,6 +22,7 @@ VALUES ('李燕容','lee2000@hexschooltest.io','USER'),
 ('Q太郎','starplatinum@hexschooltest.io','USER'),
 ('透明人','opcatiy0@hexschooltest.io','USER')
 
+
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 
 update "USER" 
